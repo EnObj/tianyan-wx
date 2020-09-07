7aae880fb6d219c4abd1d9c31e9c1fb66a5b1130
enobj <laoji52125@163.com>
2020-09-07 15:28:30 +0800

    首页支持下拉刷新

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -14,11 +14,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    db.collection('ty_user_channel').where({}).get().then(res => {
+    this.loadUserChannels()
+  },
+
+  loadUserChannels(){
+    return db.collection('ty_user_channel').where({}).get().then(res => {
       this.setData({
         userChannels: res.data
       })
-      this.check()
     })
   },
 
@@ -65,28 +68,30 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.checkIntervalId = setInterval(this.check, 1000 * 10)
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    clearInterval(this.checkIntervalId)
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    clearInterval(this.checkIntervalId)
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.loadUserChannels().then(res=>{
+      wx.stopPullDownRefresh()
+    })
   },
 
   /**
