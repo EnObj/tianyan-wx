6ad2055b02cc90e61f96202f2725c3ec9ce0f2a5
enobj <laoji52125@163.com>
2020-09-08 10:55:38 +0800

    消息通知优化

--- cloudfunctions/notifyTyMessage/index.js
+++ cloudfunctions/notifyTyMessage/index.js
@@ -6,39 +6,77 @@ cloud.init({
 })
 
 const db = cloud.database()
+const $ = db.command.aggregate
 
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
 
-  // 通过用户进行聚合
-  const {
-    data: messages
-  } = await db.collection('ty_user_channel_data_message').where({
+  const where = {
     notify: 'wait'
-  }).get()
+  }
+  while ((await db.collection('ty_user_channel_data_message').where(where).count()).total) {
 
-  // 一个一个处理
-  messages.forEach(async message => {
-    // 发消息
-    await cloud.openapi.subscribeMessage.send({
-      touser: message._openid,
-      page: '/pages/index/index',
-      data: {
-        thing1: {
-          value: '某渠道'
-        },
-        phrase2: {
-          value: '有更新'
-        }
-      },
-      templateId: '-uC7MFgpZqLROkVO_QILbH23d85gg-ErEM0KavcKP6A'
-    })
-    // 更新状态
-    await db.collection('ty_user_channel_data_message').doc(message._id).update({
-      data: {
-        notify: 'finished'
+    // 通过用户进行聚合
+    const {
+      list: userMessages
+    } = await db.collection('ty_user_channel_data_message').aggregate().match(where).group({
+      _id: '$_openid',
+      channelDatas: $.addToSet('$channelData')
+    }).end()
+
+    console.log(userMessages)
+
+    // 一个一个处理
+    for (let i = 0; i < userMessages.length; i++) {
+
+      const userMessage = userMessages[i]
+      console.log(`正在处理：${userMessage._id}`)
+
+      // 具名渠道代表
+      const channel = userMessage.channelDatas[0].channel
+      let notifyResult = 'finished'
+      try {
+        // 发消息
+        await cloud.openapi.subscribeMessage.send({
+          touser: userMessage._id,
+          page: '/pages/index/index',
+          data: {
+            thing1: {
+              value: channel.channelTemplate.name + '：' + channel.key + (userMessage.channelDatas.length > 1 ? ("等" + userMessage.channelDatas.length + "个活动") : "")
+            },
+            phrase2: {
+              value: '有更新'
+            }
+          },
+          templateId: '-uC7MFgpZqLROkVO_QILbH23d85gg-ErEM0KavcKP6A'
+        })
+        // 记录状态
+      } catch (err) {
+        console.error(err)
+        notifyResult = 'fail'
+      } finally {
+        // 更新状态
+        await db.collection('ty_user_channel_data_message').where({
+          _openid: userMessage._id,
+          ...where
+        }).update({
+          data: {
+            notify: notifyResult,
+            notifyTime: Date.now()
+          }
+        })
       }
-    })
-  })
+
+      // 把代表渠道的订阅标记关了
+      await db.collection('ty_user_channel').where({
+        _openid: userMessage._id,
+        'channel._id': channel._id
+      }).update({
+        data: {
+          notify: false
+        }
+      })
+    }
+  }
 }
