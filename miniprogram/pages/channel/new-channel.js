85f3d1e92049ad85fbe06d1693754236e0deac0a
enobj <laoji52125@163.com>
2020-09-11 09:58:37 +0800

    活动对象

--- miniprogram/pages/channel/new-channel.js
+++ miniprogram/pages/channel/new-channel.js
@@ -75,7 +75,7 @@ Page({
         attrs: this.selectedSelectors.map((selector, index)=>{
           return {
             path: selector,
-            name: `暗中观察${index+1}`
+            name: `活动对象${index+1}`
           }
         })
       }
