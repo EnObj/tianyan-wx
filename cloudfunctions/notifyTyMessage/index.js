ec6fd8ab0b90a469ec3e29011017d15dc7230068
enobj <laoji52125@163.com>
2020-09-11 17:23:23 +0800

    优化消息发送策略：高频分批处理

--- cloudfunctions/notifyTyMessage/index.js
+++ cloudfunctions/notifyTyMessage/index.js
@@ -16,68 +16,68 @@ exports.main = async (event, context) => {
     notify: 'wait',
     readed: false
   }
-  while ((await db.collection('ty_user_channel_data_message').where(where).count()).total) {
+  const {
+    total: amount
+  } = await db.collection('ty_user_channel_data_message').where(where).count()
+  console.log(`待处理数目：${amount}`)
 
-    // 通过用户进行聚合
-    const {
-      list: userMessages
-    } = await db.collection('ty_user_channel_data_message').aggregate().match(where).group({
-      _id: '$_openid',
-      channelDatas: $.addToSet('$channelData')
-    }).end()
+  // 通过用户进行聚合
+  const {
+    list: userMessages
+  } = await db.collection('ty_user_channel_data_message').aggregate().match(where).group({
+    _id: '$_openid',
+    channelDatas: $.addToSet('$channelData')
+  }).limit(50).end()
 
-    console.log(userMessages)
+  // 一个一个处理
+  for (let i = 0; i < userMessages.length; i++) {
 
-    // 一个一个处理
-    for (let i = 0; i < userMessages.length; i++) {
+    const userMessage = userMessages[i]
+    console.log(`正在处理：${userMessage._id}`)
 
-      const userMessage = userMessages[i]
-      console.log(`正在处理：${userMessage._id}`)
-
-      // 具名渠道代表
-      const channel = userMessage.channelDatas[0].channel
-      let notifyResult = 'finished'
-      try {
-        // 发消息
-        await cloud.openapi.subscribeMessage.send({
-          touser: userMessage._id,
-          page: '/pages/index/index',
-          data: {
-            thing1: {
-              value: channel.name + (userMessage.channelDatas.length > 1 ? ("等" + userMessage.channelDatas.length + "个活动") : "")
-            },
-            phrase2: {
-              value: '有更新'
-            }
-          },
-          templateId: '-uC7MFgpZqLROkVO_QILbH23d85gg-ErEM0KavcKP6A'
-        })
-        // 记录状态
-      } catch (err) {
-        console.error(err)
-        notifyResult = 'fail'
-      } finally {
-        // 更新状态
-        await db.collection('ty_user_channel_data_message').where({
-          _openid: userMessage._id,
-          ...where
-        }).update({
-          data: {
-            notify: notifyResult,
-            notifyTime: Date.now()
-          }
-        })
-      }
-
-      // 把代表渠道的订阅标记关了
-      await db.collection('ty_user_channel').where({
-        _openid: userMessage._id,
-        'channel._id': channel._id
-      }).update({
+    // 具名渠道代表
+    const channel = userMessage.channelDatas[0].channel
+    const notifyUpdater = {
+      notifyResult: 'finished',
+      notifyTime: Date.now()
+    }
+    try {
+      // 发消息
+      await cloud.openapi.subscribeMessage.send({
+        touser: userMessage._id,
+        page: '/pages/index/index',
         data: {
-          notify: false
-        }
+          thing1: {
+            value: channel.name + (userMessage.channelDatas.length > 1 ? ("等" + userMessage.channelDatas.length + "个活动") : "")
+          },
+          phrase2: {
+            value: '有更新'
+          }
+        },
+        templateId: '-uC7MFgpZqLROkVO_QILbH23d85gg-ErEM0KavcKP6A'
+      })
+      // 记录状态
+    } catch (err) {
+      console.error(err)
+      notifyUpdater.notifyResult = 'fail'
+    } finally {
+      // 更新状态
+      await db.collection('ty_user_channel_data_message').where({
+        _openid: userMessage._id,
+        ...where
+      }).update({
+        data: notifyUpdater
       })
     }
+
+    // 把代表渠道的订阅标记关了
+    await db.collection('ty_user_channel').where({
+      _openid: userMessage._id,
+      'channel._id': channel._id
+    }).update({
+      data: {
+        notify: false
+      }
+    })
   }
 }
