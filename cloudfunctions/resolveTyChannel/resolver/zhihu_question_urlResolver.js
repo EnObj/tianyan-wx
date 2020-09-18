ba1f79a43beeae8ef323279eefc049b73edeef4f
enobj <laoji52125@163.com>
2020-09-18 14:51:44 +0800

    优化zhihuUrlResolver体验

--- cloudfunctions/resolveTyChannel/resolver/zhihu_question_urlResolver.js
+++ cloudfunctions/resolveTyChannel/resolver/zhihu_question_urlResolver.js
@@ -3,20 +3,32 @@ const cheerio = require('cheerio')
 
 module.exports = {
   resolve: async function(key){
-    let result = {
-      resourceUrl: `https://www.zhihu.com/question/${key}`,
-      openResourceUrl: `https://www.zhihu.com/question/${key}`
-    }
+    const result = {}
     // 支持输入资源地址
     if(/^https?:\/\/www\.zhihu\.com\/question\/(\d+)/.test(key)){
       result.resourceUrl = result.openResourceUrl = `https://www.zhihu.com/question/${RegExp.$1}`
+    }else if(/^\d+$/.test(key)){
+      result.resourceUrl = result.openResourceUrl = `https://www.zhihu.com/question/${key}`
+    } else{
+      return {
+        errCode: 401,
+        errMsg: '请重新输入'
+      }
     }
 
     // 得到标题
     const html = await resolverUtils.request(result.resourceUrl)
     const $ = cheerio.load(html)
-    result.channelName = $('title').text().trim() || '未知'
+    const title = $('h1.QuestionHeader-title').text().trim()
+    if(title){
+      result.channelName = title
+      return result
+    }
 
-    return result
+    return {
+      errCode: 404,
+      errMsg: '未发现话题'
+    }
+
   }
 }
