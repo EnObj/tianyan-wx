dda3d43eeaea341cf3c09ac86ae822f21182bb01
enobj <laoji52125@163.com>
2020-09-30 17:42:59 +0800

    优化去除事务控制

--- cloudfunctions/refreshTyUserProfile/index.js
+++ cloudfunctions/refreshTyUserProfile/index.js
@@ -14,47 +14,42 @@ exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
   const openid = wxContext.OPENID
 
-  const {
-    fromUser = 'weixin'
-  } = event
+  console.log('openid', openid)
 
-  // 开启事务处理
-  const userProfile = await db.runTransaction(async transaction => {
+  const { fromUser } = event
 
-    // 查询用户信息
-    let {
-      data: userProfile
-    } = await transaction.collection('ty_user_profile').doc(openid).get()
-  
-    // 用户记录不存在，创建用户
-    if (!userProfile) {
-      userProfile = {
-        _id: openid,
-        _openid: openid,
-        fromUser,
-        channelLimit: 20,
-        invitedUserCount: 0,
-        createTime: Date.now()
-      }
-      await transaction.collection('ty_user_profile').add({
-        data: userProfile
-      })
+  // 查询用户信息
+  let {
+    data: userProfile
+  } = await db.collection('ty_user_profile').doc(openid).get()
 
-      if(fromUser){
-        console.log(`thanks fromUser: ${fromUser}`)
-  
-        // 感谢fromUser
-        await transaction.collection('ty_user_profile').doc(fromUser).update({
-          data: {
-            channelLimit: db.command.inc(1),
-            invitedUserCount: db.command.inc(1)
-          }
-        })
-      }
+  // 用户记录不存在，创建用户（因为_id=openid，所以不必担心因并发而重复创建同一个用户，因此此处无需再开启事务）
+  if (!userProfile) {
+    console.log('创建新用户')
+    userProfile = {
+      _id: openid,
+      _openid: openid,
+      fromUser: fromUser || 'self',
+      channelLimit: 20,
+      invitedUserCount: 0,
+      createTime: Date.now()
     }
+    await db.collection('ty_user_profile').add({
+      data: userProfile
+    })
 
-    return userProfile
-  })
+    if (fromUser && fromUser != openid) {
+      console.log(`thanks fromUser: ${fromUser}`)
+
+      // 感谢fromUser
+      await db.collection('ty_user_profile').doc(fromUser).update({
+        data: {
+          channelLimit: db.command.inc(1),
+          invitedUserCount: db.command.inc(1)
+        }
+      })
+    }
+  }
 
   return {
     userProfile
